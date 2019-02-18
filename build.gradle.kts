e7e905f90a94c3f55c65dc2c0fa980e17587004a
Irina Turova <turovaia@yandex.ru>
2019-02-18 09:14:24 +0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.11"
+}
+
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
